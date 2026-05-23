fb48e55526a1126b5fcab0d5ae89d6f2c43ce1b5
Harish <harshethari.28@gmail.com>
2026-05-23 17:16:58 +0530

    Deploy all components to clientsortingprjt

---
+++ force-app/main/default/lwc/lwcPractice/lwcPeactise.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class LwcPeactise extends LightningElement {
+
+    var name = 'Harish'
+    console.log(name)
+    var name = 'Hema'
+    console.log(name)
+
+
+}
